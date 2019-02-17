33b77e1edfbcf7e28745557f4ad5a9e518e71be6
Liam Öberg <liamoberg@Liams-MacBook-Pro.local>
2019-02-17 17:44:09 +0100

    update on init commit, start to header

---
+++ src/Components/Games/games.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './games.css';
+
+class Games extends Component {
+    render () {
+        return (
+
+        );
+    }
+}
+
+export default Games;
