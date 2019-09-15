968c7dfb0c06b0331c6874cbc14c895d06a384b5
Liam Öberg <liam@Liams-MacBook-Pro.local>
2019-09-16 00:51:02 +0200

    rewrote games/gameresult as functions

--- src/Components/GameResult/GameResult.js
+++ src/Components/GameResult/GameResult.js
@@ -3,24 +3,26 @@ import "./GameResult.css";
 import Scorers from "../Scorers/Scorers";
 import ThreeStars from "../Games/ThreeStars";
 
-const GameResult = ({ homeTeamScore, awayTeamScore, scorers, stars }) => (
-  <div className="result">
-    <div className="score">
-      <span>{homeTeamScore}</span>
-      <span> - </span>
-      <span>{awayTeamScore}</span>
+function GameResult(props) {
+  return (
+    <div className="result">
+      <div className="score">
+        <span>{props.homeTeamScore}</span>
+        <span> - </span>
+        <span>{props.awayTeamScore}</span>
+      </div>
+      <div className="scorers">
+        {props.scorers.map(scorer => (
+          <Scorers scorers={scorer} />
+        ))}
+      </div>
+      <div className="three-stars">
+        {props.stars.map(star => (
+          <ThreeStars stars={star} />
+        ))}
+      </div>
     </div>
-    <div className="scorers">
-      {scorers.map(scorer => (
-        <Scorers scorers={scorer} />
-      ))}
-    </div>
-    <div className="three-stars">
-      {stars.map(star => (
-        <ThreeStars stars={star} />
-      ))}
-    </div>
-  </div>
-);
+  );
+}
 
 export default GameResult;
--- src/Components/Games/games.js
+++ src/Components/Games/games.js
@@ -1,73 +1,117 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "./games.css";
 import Logo1 from "../../Assets/images/sprite-large.png";
 import Logo2 from "../../Assets/images/sprite-large-1.png";
 import VideoOverlay from "../VideoOverlay/VideoOverlay";
 import GameResult from "../GameResult/GameResult";
 
-class Games extends Component {
-  state = {
-    showResult: false
-  };
+function Games(props) {
+  const [showResult, toggleResult] = useState(props.showAllResults);
 
-  showResult() {
-    if (!this.state.showResult === true) {
-      this.setState({ showResult: true });
-    } else {
-      this.setState({ showResult: false });
-    }
-  }
+  useEffect(() => {
+    toggleResult(props.showAllResults);
+  }, [props.showAllResults]);
 
-  // Implement another method
-  componentWillReceiveProps(nextProps) {
-    if (this.props !== nextProps) {
-      this.setState({
-        showResult: nextProps.showAllResults
-      });
-    }
-  }
-
-  render() {
-    return (
-      <div className="game">
-        <div className="top">
-          <div className="hometeam">
-            <img src={Logo1} alt="123" />
-            <span>{this.props.games.homeTeam}</span>
-          </div>
-          <div className="resultwrap">
-            <a href="#">
-              <VideoOverlay url={this.props.games.url} />
-            </a>
-            <div
-              className={`standard-btn ${
-                this.state.showResult ? "active" : ""
-              }`}
-              onClick={() => {
-                this.showResult();
-              }}
-            >
-              {this.state.showResult ? "Hide" : "Results"}
-            </div>
-          </div>
-          <div className="awayteam">
-            <img src={Logo2} alt="123" />
-            <span>{this.props.games.awayTeam}</span>
+  return (
+    <div className="game">
+      <div className="top">
+        <div className="hometeam">
+          <img src={Logo1} alt="123" />
+          <span>{props.games.homeTeam}</span>
+        </div>
+        <div className="resultwrap">
+          <a href="#">
+            <VideoOverlay url={props.games.url} />
+          </a>
+          <div
+            className={`standard-btn ${showResult ? "active" : ""}`}
+            onClick={() => toggleResult(!showResult)}
+          >
+            {showResult ? "Hide" : "Results"}
           </div>
         </div>
-        <div className="bottom">
-          {this.state.showResult && (
-            <GameResult
-              homeTeamScore={this.props.games.homeGoals}
-              awayTeamScore={this.props.games.awayGoals}
-              scorers={this.props.games.scorers}
-              stars={this.props.games.stars}
-            />
-          )}
+        <div className="awayteam">
+          <img src={Logo2} alt="123" />
+          <span>{props.games.awayTeam}</span>
         </div>
       </div>
-    );
-  }
+      <div className="bottom">
+        {showResult && (
+          <GameResult
+            homeTeamScore={props.games.homeGoals}
+            awayTeamScore={props.games.awayGoals}
+            scorers={props.games.scorers}
+            stars={props.games.stars}
+          />
+        )}
+      </div>
+    </div>
+  );
 }
+
+// class Games extends Component {
+//   state = {
+//     showResult: false
+//   };
+
+//   showResult() {
+//     if (!this.state.showResult === true) {
+//       this.setState({ showResult: true });
+//     } else {
+//       this.setState({ showResult: false });
+//     }
+//   }
+
+//   // Implement another method
+//   componentWillReceiveProps(nextProps) {
+//     if (this.props !== nextProps) {
+//       this.setState({
+//         showResult: nextProps.showAllResults
+//       });
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <div className="game">
+//         <div className="top">
+//           <div className="hometeam">
+//             <img src={Logo1} alt="123" />
+//             <span>{this.props.games.homeTeam}</span>
+//           </div>
+//           <div className="resultwrap">
+//             <a href="#">
+//               <VideoOverlay url={this.props.games.url} />
+//             </a>
+//             <div
+//               className={`standard-btn ${
+//                 this.state.showResult ? "active" : ""
+//               }`}
+//               onClick={() => {
+//                 this.showResult();
+//               }}
+//             >
+//               {this.state.showResult ? "Hide" : "Results"}
+//             </div>
+//           </div>
+//           <div className="awayteam">
+//             <img src={Logo2} alt="123" />
+//             <span>{this.props.games.awayTeam}</span>
+//           </div>
+//         </div>
+//         <div className="bottom">
+//           {this.state.showResult && (
+//             <GameResult
+//               homeTeamScore={this.props.games.homeGoals}
+//               awayTeamScore={this.props.games.awayGoals}
+//               scorers={this.props.games.scorers}
+//               stars={this.props.games.stars}
+//             />
+//           )}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
 export default Games;
